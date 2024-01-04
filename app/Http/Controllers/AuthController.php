24dd601a85b68a8cbb4ed1eb7aa5452f0c468da2
Rahul B <rahul.b@avdevs.com>
2024-01-03 23:05:44 -0600

    User login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\UserCreateRequest;
 use App\Services\UserService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -13,12 +15,12 @@ class AuthController extends Controller
     public function __construct(private UserService $userService)
     {
     }
-    public function createUser(UserCreateRequest $request)
+    public function create(UserCreateRequest $request)
     {
         try {
             $params = $request->only(['name', 'email', 'password']);
             $params['password'] = Hash::make($params['password']);
-            $user = $this->userService->newUser($params);
+            $user = $this->userService->createUser($params);
             if ($user) {
                 $token = $user->createToken($user->email);
                 return response()->json(['message' => 'User created successfully', 'token' => $token->plainTextToken], 200);
@@ -30,7 +32,21 @@ class AuthController extends Controller
             return response()->json(['error' => 'Something went wrong. Please try again.'], 500);
         }
     }
-    public function verifyUser()
+    public function login(LoginRequest $request)
     {
+        try {
+            $params = $request->only(['email', 'password']);
+            $params = $request->only(['email', 'password']);
+            if (Auth::attempt($params)) {
+                $user = Auth::user();
+                $token = $user->createToken($user->email);
+                return response()->json(['message' => 'User verified successfully', 'token' => $token->plainTextToken], 200);
+            } else {
+                throw new Exception("Unable to verify user");
+            }
+        } catch (Exception $ex) {
+            //Write the logs here
+            return response()->json(['error' => 'Something went wrong. Please try again.'], 500);
+        }
     }
 }
