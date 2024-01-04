f40cb943b2ba756a81c4aca58fa26c025ddd0fca
Rahul B <rahul.b@avdevs.com>
2024-01-04 14:17:37 -0600

    Fix note search

--- database/migrations/2024_01_03_052531_create_notes_table.php
+++ database/migrations/2024_01_03_052531_create_notes_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('NO ACTION');
-            $table->text('content')->index();
+            $table->text('content');
+            $table->fullText('content');
             $table->timestamps();
         });
     }
